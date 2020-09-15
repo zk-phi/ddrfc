d0193b8a4bf70b6a8b7aa9bb7cf716f7e6202303
zk-phi <zk-phi@users.noreply.github.com>
2020-09-15 18:35:53 +0900

    Add sort feature

--- main.js
+++ main.js
@@ -1,6 +1,30 @@
+function cmp (a, b) {
+    return a < b ? -1 : a > b ? 1 : 0;
+}
+
+function sortByTitle (levels) {
+    return levels.map(function (level) {
+        return {
+            level: level.level,
+            charts: level.charts.sort(function (a, b) { return cmp(a.title, b.title); })
+        };
+    });
+}
+
+function sortByClear (levels) {
+    const sv = { mfc: 0, pfc: 1, gfc: 2, fc: 3, ex: 4, clear: 5, fail: 6, noplay: 7 };
+    return levels.map(function (level) {
+        return {
+            level: level.level,
+            charts: level.charts.sort(function (a, b) { return cmp(sv[a.status], sv[b.status]); })
+        };
+    });
+}
+
 const vm = new Vue({
     el: "#app",
     data: {
+        sort: "status",
         selected: null,
         levels: [],
         error: null
@@ -8,7 +32,7 @@ const vm = new Vue({
     mounted: function () {
         var match = location.href.match(/\?(.+)$/);
         var xhr   = new XMLHttpRequest();
-        xhr.onload = function () { vm.levels = JSON.parse(xhr.responseText); };
+        xhr.onload = function () { vm.levels = sortByClear(JSON.parse(xhr.responseText)); };
         xhr.onerror = function () { vm.error = "Error loading chart list"; };
         xhr.open("GET", match ? match[1] : "levels.json", true);
         xhr.send(null);
@@ -35,6 +59,11 @@ const vm = new Vue({
                 };
             });
             return summaries;
+        }
+    },
+    watch: {
+        sort: function () {
+            vm.levels = (vm.sort == "title" ? sortByTitle : sortByClear)(vm.levels);
         }
     },
     methods: {
